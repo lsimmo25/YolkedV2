b9fd8cdc5a617698b3088f7be114388774cfbf8a
Lyle Simmons <lylesimmons125@gmail.com>
2024-08-05 21:47:12 -0400

    added bodyweight and food model

---
+++ YolkedWorkoutLogger.Server/Models/BodyWeight.cs
@@ -0,0 +1,9 @@
+﻿namespace YolkedWorkoutLogger.Server.Models
+{
+    public class BodyWeight
+    {
+        public int Id { get; set; }
+        public int Weight { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ YolkedWorkoutLogger.Server/Models/Food.cs
@@ -0,0 +1,10 @@
+﻿namespace YolkedWorkoutLogger.Server.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Calories { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
