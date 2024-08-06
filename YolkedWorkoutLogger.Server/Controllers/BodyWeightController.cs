978849687c1354546bd2e0463a367c27be4eff4a
Lyle Simmons <lylesimmons125@gmail.com>
2024-08-06 18:12:13 -0400

    added food / bodyweight controllers

--- YolkedWorkoutLogger.Server/Controllers/BodyWeightController.cs
+++ YolkedWorkoutLogger.Server/Controllers/BodyWeightController.cs
@@ -1,12 +1,100 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YolkedWorkoutLogger.Server.Models;
 
 namespace YolkedWorkoutLogger.Server.Controllers
 {
-    public class BodyWeightController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BodyWeightController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public BodyWeightController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBodyWeights()
+        {
+            var bodyWeights = await _context.BodyWeights.ToListAsync();
+            return Ok(bodyWeights);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBodyWeightById(int id)
+        {
+            var bodyWeight = await _context.BodyWeights.FirstOrDefaultAsync(bw => bw.Id == id);
+            if (bodyWeight == null)
+            {
+                return NotFound();
+            }
+            return Ok(bodyWeight);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBodyWeight([FromBody] BodyWeight bodyWeight)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BodyWeights.Add(bodyWeight);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBodyWeightById), new { id = bodyWeight.Id }, bodyWeight);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBodyWeight(int id, [FromBody] BodyWeight bodyWeight)
+        {
+            if (id != bodyWeight.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Entry(bodyWeight).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.BodyWeights.Any(bw => bw.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBodyWeight(int id)
+        {
+            var bodyWeight = await _context.BodyWeights.FindAsync(id);
+            if (bodyWeight == null)
+            {
+                return NotFound();
+            }
+
+            _context.BodyWeights.Remove(bodyWeight);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- YolkedWorkoutLogger.Server/Controllers/FoodController.cs
+++ YolkedWorkoutLogger.Server/Controllers/FoodController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using YolkedWorkoutLogger.Server.Models;
@@ -7,10 +8,11 @@ using YolkedWorkoutLogger.Server.Models;
 namespace YolkedWorkoutLogger.Server.Controllers
 {
     [ApiController]
-    [Route("/api/[controller]")]
+    [Route("api/[controller]")]
     public class FoodController : ControllerBase
     {
         private readonly AppDbContext _context;
+
         public FoodController(AppDbContext context)
         {
             _context = context;
@@ -19,8 +21,8 @@ namespace YolkedWorkoutLogger.Server.Controllers
         [HttpGet]
         public async Task<IActionResult> GetFoods()
         {
-            var Foods = await _context.Foods.ToListAsync();
-            return Ok(Foods);
+            var foods = await _context.Foods.ToListAsync();
+            return Ok(foods);
         }
 
         [HttpGet("{id}")]
@@ -37,9 +39,62 @@ namespace YolkedWorkoutLogger.Server.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateFood([FromBody] Food food)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _context.Foods.Add(food);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetFoodById), new { id = food.Id }, food);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateFood(int id, [FromBody] Food food)
+        {
+            if (id != food.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Entry(food).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Foods.Any(s => s.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFood(int id)
+        {
+            var food = await _context.Foods.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            _context.Foods.Remove(food);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
